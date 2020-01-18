ea442b289a73d3c3bcddba4ed6b57b5b114db71a
iPagar <ipagar@icloud.com>
2020-01-18 15:11:09 +0300

    timeout axios patch

--- src/services/index.js
+++ src/services/index.js
@@ -4,6 +4,6 @@ const sign = window.location.search;
 
 export const api = axios.create({
 	baseURL: "https://ipagar.ddns.net:8088",
-	timeout: 1000,
+	timeout: 10000,
 	headers: { "X-Sign-Header": sign }
 });
