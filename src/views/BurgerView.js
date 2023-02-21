d3fc5febf62fbfc3698b1028b34863559330ea02
iPagar <p.garaev@beetbarrel.ru>
2023-02-22 00:29:18 +0400

    add testing

--- src/views/BurgerView.js
+++ src/views/BurgerView.js
@@ -13,6 +13,7 @@ import {
   Card,
   Link,
   Cell,
+  Div,
 } from "@vkontakte/vkui";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -33,9 +34,11 @@ import Icon28Users from "@vkontakte/icons/dist/28/users";
 import Icon28UserCircleOutline from "@vkontakte/icons/dist/28/user_circle_outline";
 import Icon20Info from "@vkontakte/icons/dist/20/info";
 import Icon24UserAdd from "@vkontakte/icons/dist/24/user_add";
-import { Icon24AppBadgeOutline } from "@vkontakte/icons";
+import { Icon24AppBadgeOutline, Icon28WrenchOutline } from "@vkontakte/icons";
 import Icon28NotificationCircleFillGray from "@vkontakte/icons/dist/28/notification_circle_fill_gray";
 import free from "../img/free.jpg";
+import Lottie from "lottie-react";
+import gearsAnimation from "../assets/gears.json";
 
 const BurgerView = ({ id }) => {
   const activePanel = useSelector((state) => state.burger.activePanel);
@@ -99,7 +102,6 @@ const BurgerView = ({ id }) => {
               Профиль
             </SimpleCell>
           }
-
           <SimpleCell
             expandable
             before={<Icon28Users />}
@@ -122,6 +124,49 @@ const BurgerView = ({ id }) => {
               </React.Fragment>
             }
           />
+          {/* {student.hasOwnProperty("student") && ( */}
+
+          <Div>
+            <div
+              style={{
+                padding: "12px 16px",
+                background: "var(--content_tint_background)",
+                boxShadow: "0 0 8px rgba(0, 0, 0, 0.15)",
+                borderRadius: 8,
+                border: "1px solid var(--image_border)",
+                maxWidth: 600,
+                position: "relative",
+              }}
+            >
+              <div
+                style={{
+                  fontSize: 16,
+                  fontWeight: 500,
+                  marginBottom: 8,
+                  color: "var(--text_primary)",
+                  zIndex: 1,
+                  position: "relative",
+                }}
+              >
+                Участвуйте в тестировании приложения и влияйте на его развитие
+              </div>
+              <div
+                style={{
+                  width: 64,
+                  position: "absolute",
+                  bottom: 0,
+                  right: 0,
+                  filter: "var(--gears)",
+                }}
+              >
+                <Lottie animationData={gearsAnimation} loop={true} />
+              </div>
+              <Link href="https://bit.ly/stankintesting" target="_blank">
+                Перейти
+              </Link>
+            </div>
+          </Div>
+          {/* )} */}
           {student.hasOwnProperty("student") && !student.notify && (
             <Banner
               before={
@@ -160,7 +205,6 @@ const BurgerView = ({ id }) => {
               }
             />
           )}
-
           {additional && !additional.isMemberGroup && (
             <Banner
               before={<Icon24UserAdd width={48} height={48} />}
