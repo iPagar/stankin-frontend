774f622f320bd2af5f5bdaeaa474ad7b8cd6f087
Pavel Garaev <ipagar@icloud.com>
2024-01-23 23:46:58 +0400

    Remove burger and schedule reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 import config from "./config";
 import init from "./init";
-import schedule from "./schedule";
-import burger from "./burger";
 import { api } from "../../api/api";
 
 export default combineReducers({
   [api.reducerPath]: api.reducer,
   config,
   init,
-  schedule,
-  burger,
 });
